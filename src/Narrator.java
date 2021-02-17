8aec43baaf90e5928a1d816892f4140a1db5f2b1
Liam Donovan <grandlembadon@gmail.com>
2021-02-16 20:14:30 -0500

    Committing work on Dungeonbattler

--- src/Narrator.java
+++ src/Narrator.java
@@ -26,6 +26,7 @@ public class Narrator {
 
                 if ((narrator.length() != 0))
                 {
+                    String dialogueLine = narrator;
                     dialogueList.add(narrator);
                 }
 
