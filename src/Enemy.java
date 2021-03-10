1693823a00924d572890506ffe7f39fb5816dffc
Liam Donovan <grandlembadon@gmail.com>
2021-03-09 20:26:55 -0500

    Committing DB work

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,10 @@
 public class Enemy extends Player {
     Narrator narrator;
 
+    public Enemy(Narrator narrator) {
+        this.narrator = narrator;
+    }
+    public Enemy() {
+        
+    }
 }
