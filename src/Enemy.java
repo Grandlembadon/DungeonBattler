57a370d7056397471d873c89e05d81aed45891f7
Liam Donovan <grandlembadon@gmail.com>
2021-03-12 20:39:30 -0500

    Committing DB work

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class Enemy extends Player {
     Narrator narrator;
+    ArrayList<Skill> skillArraylist;
 
     public Enemy(Narrator narrator) {
         this.narrator = narrator;
