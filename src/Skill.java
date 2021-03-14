b79a2083d929b510c50115da904c1639082fc4fe
Liam Donovan <grandlembadon@gmail.com>
2021-03-13 21:26:07 -0500

    Committing DB work

--- src/Skill.java
+++ src/Skill.java
@@ -1,10 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Skill {
-    String skillName;
-    int skillCost;
-    int skillDamage;
+    private String skillName;
+    private int skillCost;
+    private int skillDamage;
 
 
     public Skill() {
+
+    }
+    public static ArrayList<Skill> createWeaponList() {
+
+        BufferedReader inputStream = null;
+        ArrayList<Weapon> weaponList = new ArrayList<>();
+
+        try {
+            inputStream = new BufferedReader(new FileReader(Path.WEAPONS.path));
+
+            String weaponline;
+
+            while ((weaponline = inputStream.readLine()) != null) {
+                String [] weaponInfo = weaponline.split(",");
+                int damage = Integer.parseInt(weaponInfo[0]);
+                int critDamage = Integer.parseInt(weaponInfo[1]);
+                int critChance = Integer.parseInt(weaponInfo[2]);
+                String name = weaponInfo[3];
+                Weapon weapon = new Weapon(damage,critDamage,critChance,name);
+                weaponList.add(weapon);
+
+            }
+
+        } catch (
+                FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return weaponList;
 
     }
 
