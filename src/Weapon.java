78b9207f0735778d80748375a66b52ac9d312981
Liam Donovan <grandlembadon@gmail.com>
2021-03-21 21:39:27 -0400

    committing Db work

--- src/Weapon.java
+++ src/Weapon.java
@@ -54,10 +54,13 @@ public class Weapon {
 
     public void strike(Player user, Enemy enemy,Narrator narrator) {
         int newHP = (enemy.getCurrentHP() - user.getWeapon().damage);
+        narrator.init();
         enemy.setCurrentHP(newHP);
         if (this.name.equalsIgnoreCase("Staff")){
-            narrator.playDialogueSleep(24,0);
+            narrator.playDialogueSleep(24,3000);
 
+        } else if (this.name.equalsIgnoreCase("Sword")){
+            
         }
 
 
