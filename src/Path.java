105b6fa1153a95945104d08c7163635f0dbec243
Liam Donovan <grandlembadon@gmail.com>
2021-04-15 22:05:01 -0400

    Committing db work

--- src/Path.java
+++ src/Path.java
@@ -3,7 +3,8 @@ public enum Path {
     DIALOGUE("src/Files/Dialogue.txt"),
     CLASSES("src/Files/ClassInfo.txt"),
     SKILLS("src/Files/SKillLIst.txt"),
-    ENEMYWEAPONS("src/Files/EnemyWeaponList.txt");
+    ENEMYWEAPONS("src/Files/EnemyWeaponList.txt"),
+    ENEMYINFO("src/Files/EnemyInfo.txt");
     String path;
 
     Path(String path) {
