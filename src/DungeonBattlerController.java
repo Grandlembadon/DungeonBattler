9af311affca27c4ce352f0ea7287fd8add885e78
Liam Donovan <grandlembadon@gmail.com>
2021-04-18 22:34:02 -0400

    Committing DB work

--- src/DungeonBattlerController.java
+++ src/DungeonBattlerController.java
@@ -89,7 +89,8 @@ public class DungeonBattlerController {
 
 
 
-            Enemy SkeletonSoldier = new Enemy("SkeletonSoldier", enemyWeaponList.get(0));
+            Enemy SkeletonSoldier = new Enemy (enemyList.get(0).getEnemyHP(),enemyList.get(0).getEnemySP(),
+                    enemyList.get(0).getEnemyName(),enemyWeaponList.get(0));
             ArrayList<Skill> EnemySkills = new ArrayList();
             SkeletonSoldier.setEnemySkills(EnemySkills);
             SkeletonSoldier.getEnemySkills().add(skillList.get(3));
