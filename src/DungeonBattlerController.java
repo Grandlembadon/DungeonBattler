faf3458a0db84a8dcd68dac52e387bde48a2f839
Liam Donovan <grandlembadon@gmail.com>
2021-05-23 23:20:45 -0400

    COmmitting DB work

--- src/DungeonBattlerController.java
+++ src/DungeonBattlerController.java
@@ -161,6 +161,7 @@ public class DungeonBattlerController {
             narrator.playDialogueSleep(37, 3000);
             narrator.playDialogueSleep(37, 3000);
             narrator.playDialogueSleep(37, 3000);
+            narrator.playDialogueSleep(37, 3000);
 
 
 
