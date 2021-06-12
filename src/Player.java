e91bd70dc24598a6b5afe78e99467bd8cbfe97dc
Liam Donovan <grandlembadon@gmail.com>
2021-06-11 22:17:22 -0400

    Committing DB work

--- src/Player.java
+++ src/Player.java
@@ -168,7 +168,7 @@ public class Player {
 
                 String response2 = scanner2.next();
 
-                if (response2.equalsIgnoreCase(this.getPlayerSkills()){
+                if (response2.equalsIgnoreCase(this.getPlayerSkills().get())){
 
                 }
             }
