9b8c5d4b94e1e2239a5b2f21a51adecf883775f8
Liam Donovan <grandlembadon@gmail.com>
2021-06-18 21:29:20 -0400

    Committing DB work

--- src/Player.java
+++ src/Player.java
@@ -183,6 +183,7 @@ public class Player {
                 }
             } else if (response.equalsIgnoreCase("defend")){
                 this.defend(narrator,isDefending);
+                validCommand = true;
 
             }
         }
