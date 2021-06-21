145f20706c086b86826b1ef25153310eaebf447d
Liam Donovan <grandlembadon@gmail.com>
2021-06-20 23:03:42 -0400

    Committing DB work

--- src/Item.java
+++ src/Item.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Item {
     private String itemName;
     private int quantity;
@@ -35,6 +41,38 @@ public class Item {
 
     }
     public void sellItem(){
+
+    }
+    public static ArrayList<Skill> createSkillList() {
+
+        BufferedReader inputStream = null;
+        ArrayList<Skill> skillList = new ArrayList<>();
+
+        try {
+            inputStream = new BufferedReader(new FileReader(Path.SKILLS.path));
+
+            String skillLine;
+
+            while ((skillLine = inputStream.readLine()) != null) {
+                String [] skillInfo = skillLine.split(",");
+                String skillName = skillInfo[0];
+                int skillCost = Integer.parseInt(skillInfo[1]);
+                int skillDamage = Integer.parseInt(skillInfo[2]);
+                Skill skill = new Skill(skillName,skillCost,skillDamage);
+                skillList.add(skill);
+
+            }
+
+        } catch (
+                FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return skillList;
 
     }
 
