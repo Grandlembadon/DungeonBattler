3ccb3248f7ac2e6d89a25a8e5cabb59366b9ec4f
Liam Donovan <grandlembadon@gmail.com>
2021-06-30 22:55:20 -0400

    Committing DB work

--- src/DungeonBattlerController.java
+++ src/DungeonBattlerController.java
@@ -164,6 +164,7 @@ public class DungeonBattlerController {
             SkeletonSoldier.setCoolDown2(true);
             boolean battleOver = false;
             narrator.playDialogueSleep(37, 3000);
+            narrator.playDialogueSleep(37, 3000);
 
 
             while (user.getCurrentHP() > 0 || SkeletonSoldier.getEnemyCurrentHP() > 0) {
